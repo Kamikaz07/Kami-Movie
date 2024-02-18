2ed2522f7f368e4fb1280e003d066791de340bf8
Kamikaz07 <pardalfon@gmail.com>
2024-02-18 18:42:28 +0000

    done

---
+++ src/setup/constants.ts
@@ -0,0 +1,7 @@
+export const APP_VERSION = import.meta.env.PACKAGE_VERSION;
+export const DISCORD_LINK = "https://discord.movie-web.app";
+export const LINKEDIN_LINK = "https://linkedin.com/in/rodrigo-fonseca-kamiweb";
+export const GITHUB_LINK = "https://github.com/Kamikaz07";
+export const DONATION_LINK = "https://ko-fi.com/kamiweb";
+export const GA_ID = "G-44YVXRL61C";
+export const BACKEND_URL = "https://backend.movie-web.app";
